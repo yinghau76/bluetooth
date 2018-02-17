1b05d34d8bfb3b9b6ab3f8b4af022217d0a8640a
Patrick Tsai <yinghau76@gmail.com>
2018-02-17 01:17:29 -0800

    Show scan results in list

--- app/src/main/java/com/simplypatrick/gattclient/DeviceScanActivity.kt
+++ app/src/main/java/com/simplypatrick/gattclient/DeviceScanActivity.kt
@@ -1,6 +1,7 @@
 package com.simplypatrick.gattclient
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
@@ -8,16 +9,49 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.device_item_row.*
+import kotlinx.android.synthetic.main.device_item_row.view.*
+import kotlinx.coroutines.experimental.launch
 
 class DeviceScanActivity : AppCompatActivity() {
-    lateinit var mBluetoothAdapter: BluetoothAdapter
+    private lateinit var mBluetoothAdapter: BluetoothAdapter
+    private val mDevices = HashMap<BluetoothDevice, ScanResult>()
+
+    private lateinit var linearLayoutManager: LinearLayoutManager
+
+    class DeviceHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        fun bindDevice(scanResult: ScanResult) {
+            view.name.text = scanResult.device.name ?: scanResult.device.address
+        }
+    }
+
+    class RecyclerAdapter(val scanResults: Array<ScanResult>) : RecyclerView.Adapter<DeviceHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DeviceHolder {
+            val inflatedView = parent.inflate(R.layout.device_item_row, false)
+            return DeviceHolder(inflatedView)
+        }
+
+        override fun getItemCount() = scanResults.size
+
+        override fun onBindViewHolder(holder: DeviceHolder, position: Int) {
+            holder.bindDevice(scanResults[position])
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        linearLayoutManager = LinearLayoutManager(this)
+        deviceList.layoutManager = linearLayoutManager
 
         // Use this check to determine whether BLE is supported on the device.  Then you can
         // selectively disable BLE-related features.
@@ -29,6 +63,11 @@ class DeviceScanActivity : AppCompatActivity() {
 
         val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         mBluetoothAdapter = bluetoothManager.adapter
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopScanLeDevices()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -44,16 +83,30 @@ class DeviceScanActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    private val mLeScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+
+            println("$callbackType $result")
+            mDevices[result.device] = result
+
+            updateDeviceList()
+        }
+    }
+
     private fun startScanLeDevices() {
-        val scanner = mBluetoothAdapter.bluetoothLeScanner
-        scanner.startScan(object : ScanCallback() {
-            override fun onScanResult(callbackType: Int, result: ScanResult) {
-                super.onScanResult(callbackType, result)
-                println("${callbackType} ${result}")
-            }
-        })
+        launch {
+            mBluetoothAdapter.bluetoothLeScanner.startScan(mLeScanCallback)
+        }
     }
 
     private fun stopScanLeDevices() {
+        launch {
+            mBluetoothAdapter.bluetoothLeScanner.stopScan(mLeScanCallback)
+        }
+    }
+
+    private fun updateDeviceList() {
+        deviceList.adapter = RecyclerAdapter(mDevices.values.toTypedArray())
     }
 }
